8d8d2a7a31fdc3c16938df605bf796015ed40ac6
Qiushi Li <ql440@nyu.edu>
2019-05-28 21:46:16 -0400

    Initial Commit

---
+++ QishiDesignPattern/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  QishiDesignPattern
+//
+//  Created by Qiushi Li on 5/28/19.
+//  Copyright © 2019 Qiushi Li. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
