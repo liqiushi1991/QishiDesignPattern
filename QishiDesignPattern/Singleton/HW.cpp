df027970cce912701329e6c4ef0b90aee5184cf4
liqiushi1991 <ql440@nyu.edu>
2019-06-08 23:55:59 -0400

    HW

---
+++ QishiDesignPattern/Singleton/HW.cpp
@@ -0,0 +1,14 @@
+#include <functional>
+using namespace std;
+
+struct SingletonTester
+{
+    template <typename T>
+    bool is_singleton(function<T*()> factory)
+    {
+        auto a = factory();
+        auto b = factory();
+        
+        return a==b;
+    }
+};
